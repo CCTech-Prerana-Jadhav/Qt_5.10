33a514eab4933c7b8cbcdfd84ed5a630a898748f
Narayan Hamde <narayan.hamde@cctech.co.in>
2024-10-29 12:19:51 +0530

    Bug Fix

--- Visualizer/src/OBJReader.cpp
+++ Visualizer/src/OBJReader.cpp
@@ -9,9 +9,9 @@
 #include <vector>
 #include <sstream>
 #include <cassert>
+#include <string>
 #include <map>
 
-using namespace std;
 
 OBJReader::OBJReader()
 {
@@ -21,94 +21,103 @@ OBJReader::~OBJReader()
 {
 }
 
-void OBJReader::read(const string& fileName, Triangulation& triangulation)
+void OBJReader::read(const std::string& fileName, Triangulation& triangulation)
 {
-    map<double, int> uniqueMap;
+    std::map<double, int> uniqueMap;
     double xyz[3];
     double normalXYZ[3];
-    string str1;
-    string str2;
-    string str3;
+    std::string str1;
+    std::string str2;
+    std::string str3;
     //vector<int> pointIndices;
-    vector<Point> vertices;
-    vector<Point> normals;
+    std::vector<Point> vertices;
+    std::vector<Point> normals;
 
 
-    ifstream infile(fileName);
+    std::ifstream infile(fileName);
     //assert(infile && "Error: Could not open file");
     if (infile.is_open())
     {
-
-        string line;
+        std::string line;
 
         while (getline(infile, line))
         {
-            stringstream ss(line);
-            string word;
-            while (ss >> word)
+            std::stringstream ss(line);
+            QString _line = QString::fromStdString(line);
+            QStringList linelist = _line.split(" ");
+            //std::cout << linelist.value(0).toStdString() << std::endl;
+            if (linelist.value(0) == "v")
             {
-                if (word == "v")
+				xyz[0] = linelist.value(1).toDouble();
+                xyz[1] = linelist.value(2).toDouble();
+                xyz[2] = linelist.value(3).toDouble();
+
+                int pt[3];
+
+                for (int i = 0; i < 3; i++)
                 {
-                    ss >> xyz[0] >> xyz[1] >> xyz[2];
-
-                    int pt[3];
-
-                    for (int i = 0; i < 3; i++)
+                    auto pair = uniqueMap.find(xyz[i]);
+                    if (pair == uniqueMap.end())
                     {
-                        auto pair = uniqueMap.find(xyz[i]);
-                        if (pair == uniqueMap.end())
-                        {
-                            triangulation.UniqueNumbers.push_back(xyz[i]);
-                            uniqueMap[xyz[i]] = triangulation.UniqueNumbers.size() - 1;
-                            pt[i] = triangulation.UniqueNumbers.size() - 1;
-
-                        }
-                        else
-                        {
-                            pt[i] = pair->second;
-
-                        }
+                        triangulation.UniqueNumbers.push_back(xyz[i]);
+                        uniqueMap[xyz[i]] = triangulation.UniqueNumbers.size() - 1;
+                        pt[i] = triangulation.UniqueNumbers.size() - 1;
 
                     }
-                    vertices.push_back(Point(pt[0], pt[1], pt[2]));
-                }
-
-                if (word == "vn")
-                {
-                    ss >> normalXYZ[0] >> normalXYZ[1] >> normalXYZ[2];
-
-                    int pt[3];
-
-                    for (int i = 0; i < 3; i++)
+                    else
                     {
-                        auto pair = uniqueMap.find(normalXYZ[i]);
-                        if (pair == uniqueMap.end())
-                        {
-                            triangulation.UniqueNumbers.push_back(normalXYZ[i]);
-                            uniqueMap[normalXYZ[i]] = triangulation.UniqueNumbers.size() - 1;
-                            pt[i] = triangulation.UniqueNumbers.size() - 1;
+                        pt[i] = pair->second;
 
-                        }
-                        else
-                        {
-                            pt[i] = pair->second;
-                        }
                     }
-                    normals.push_back(Point(pt[0], pt[1], pt[2]));
-                }
 
-                if (word == "f")
+                }
+                vertices.push_back(Point(pt[0], pt[1], pt[2]));
+            }
+
+            if (linelist.value(0) == "vn")
+            {
+                normalXYZ[0] = linelist.value(1).toDouble();
+                normalXYZ[1] = linelist.value(2).toDouble();
+                normalXYZ[2] = linelist.value(3).toDouble();
+
+                int pt[3];
+
+                for (int i = 0; i < 3; i++)
                 {
-                    ss >> str1 >> str2 >> str3;
-                    QString str_s1 = QString::fromStdString(str1);
-                    QString str_s2 = QString::fromStdString(str2);
-                    QString str_s3 = QString::fromStdString(str3);
-                    QStringList splitlist1 = str_s1.split("/");
-                    QStringList splitlist2 = str_s2.split("/");
-                    QStringList splitlist3 = str_s3.split("/");
+                    auto pair = uniqueMap.find(normalXYZ[i]);
+                    if (pair == uniqueMap.end())
+                    {
+                        triangulation.UniqueNumbers.push_back(normalXYZ[i]);
+                        uniqueMap[normalXYZ[i]] = triangulation.UniqueNumbers.size() - 1;
+                        pt[i] = triangulation.UniqueNumbers.size() - 1;
 
-                    triangulation.Triangles.push_back(Triangle(normals[splitlist1.value(2).toInt() - 1], vertices[splitlist1.value(0).toInt() - 1], vertices[splitlist2.value(0).toInt() - 1], vertices[splitlist3.value(0).toInt() - 1]));
+                    }
+                    else
+                    {
+                        pt[i] = pair->second;
+                    }
                 }
+                normals.push_back(Point(pt[0], pt[1], pt[2]));
+            }
+
+            if (linelist.value(0) == "f")
+            {
+                std::string fLetter;
+                ss >> fLetter >> str1 >> str2 >> str3;
+                QString str_s1 = QString::fromStdString(str1);
+                QString str_s2 = QString::fromStdString(str2);
+                QString str_s3 = QString::fromStdString(str3);
+                QStringList splitList =  str_s1.split("/");
+                splitList << str_s2.split("/") << str_s3.split("/");
+                int firstVertexId = splitList.value(0).toInt() - 1;
+                int secondVertexId = splitList.value(3).toInt() - 1;
+                int thirdVertexId = splitList.value(6).toInt() - 1;
+
+                for (int i = 0; i < splitList.size(); i++)
+                Point v1 = vertices[firstVertexId];
+                Point v2 = vertices[secondVertexId];
+                Point v3 = vertices[thirdVertexId];
+                triangulation.Triangles.push_back(Triangle(normals[splitList.value(2).toInt()-1], vertices[firstVertexId], vertices[secondVertexId], vertices[thirdVertexId]));
             }
         }
     }
